b3536858bf1845b26e686f4a7e71241361a45f8c
pavansai018 <18pavansai@gmail.com>
2026-07-01 12:18:06 +0800

    removed uniform threshold velocity command cfg

--- source/M20_Piper/M20_Piper/tasks/manager_based/m20_piper/m20_piper_env_cfg.py
+++ source/M20_Piper/M20_Piper/tasks/manager_based/m20_piper/m20_piper_env_cfg.py
@@ -88,22 +88,6 @@ class M20PiperSceneCfg(InteractiveSceneCfg):
 # MDP settings
 ##
 
-@configclass
-class CommandsCfg:
-    """Command specifications for the MDP."""
-
-    base_velocity = mdp.UniformThresholdVelocityCommandCfg(
-        asset_name="robot",
-        resampling_time_range=(10.0, 10.0),
-        rel_standing_envs=0.02,
-        rel_heading_envs=1.0,
-        heading_command=True,
-        heading_control_stiffness=0.5,
-        debug_vis=False,
-        ranges=mdp.UniformThresholdVelocityCommandCfg.Ranges(
-            lin_vel_x=(-1.0, 1.0), lin_vel_y=(-1.0, 1.0), ang_vel_z=(-1.0, 1.0), heading=(-math.pi, math.pi)
-        ),
-    )
 
 
 @configclass
@@ -626,7 +610,6 @@ class M20PiperEnvCfg(ManagerBasedRLEnvCfg):
     # Basic settings
     observations: ObservationsCfg = ObservationsCfg()
     actions: ActionsCfg = ActionsCfg()
-    commands: CommandsCfg = CommandsCfg()
     events: EventCfg = EventCfg()
     # MDP settings
     rewards: RewardsCfg = RewardsCfg()
